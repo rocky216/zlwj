eeb1daa1fba6c4c4cc630e3003bdb4ecf6de30a2
rocky216 <1120247491@qq.com>
2020-09-03 10:33:00 +0800

    eer

--- gcwyzlwj/lib/config/base.dart
+++ gcwyzlwj/lib/config/base.dart
@@ -1,3 +1,3 @@
-final baseUrl = "http://192.168.1.6/mobile";
-final baseResources = "http://192.168.1.6";
+final baseUrl = "http://192.168.0.117/mobile";
+final baseResources = "http://192.168.0.117";
 final amapKey = "bd6e7c2182f28d71cc59ca386c18dab2";
